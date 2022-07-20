a4c8c0612a219a6520c0ddf010ee7d193b44224d
PRINCE KUMABIO <kumabio.prince@student.moringaschool.com>
2022-07-20 00:38:47 +0000

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Never thought I could one day be coding react. Where I am now, Giving up is not a option at all</p>
+    <img src={image} alt="I made this"></img>
+    
+    </div>;
 }
 
 export default About;
