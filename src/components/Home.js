c7662258c56b4b6e195b60ee885307706230bf5f
PRINCE KUMABIO <kumabio.prince@student.moringaschool.com>
2022-07-27 08:55:24 +0000

    Assignment Completed With the various errors Corrected

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div id="home">
+  return (
 
-    <h1 style={{color:'firebrick'}}> {name} is a Web Developer from Your {city}</h1>;
+    <div id="home">
 
-  </div>;
+      <h1 style={{ color: "firebrick" }}>{name} is a Web Developer from {city} </h1>
+
+    </div>
+  );
+
 }
 
 export default Home;
